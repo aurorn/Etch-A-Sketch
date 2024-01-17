fc55ce54f212f16f3ed4388ae84b0562c33c9fac
aurorn <aurornispro@gmail.com>
2024-01-17 14:45:42 -0500

    updated all files to follow the flexbox version of Etch-A-Sketch

--- js/script.js
+++ js/script.js
@@ -1,114 +1,45 @@
-// Constant list
+/* Size of the Grid */
 
-const container = document.querySelector('.container');
-const colorButtons = document.querySelectorAll('.color-choice');
-const userColorPicker = document.querySelector('#input-color');
-const clearButton = document.querySelector('.clear');
-var slider = document.querySelector('#sizeRange');e
-var color = 'black';
+const gridSize = 600;
+let cellsLength = 16;
+
+/* Const's for Etch a Sketch and Slider t */
+const easArea = document.querySelector("#eas-area");
+const sliderContainer = document.querySelector("#slider-container");
+const slider = document.querySelector("#slider");
+const sliderValue = document.querySelector("#slider-value");
+/* */
+sliderValue.textContent = `${slider.value} x ${slider.value} (Resolution)`;
+easArea.style.width = easArea.style.height = `${gridSize}px`;
 
 
-
-function makeGrid (gridNumber) {
-    let gridArea = gridNumber * gridNumber;
-    for (let i = 1; i <= gridArea; i++) {
-        let gridBox = document.createElement('div');
-        container.style.gridTemplateColumns = `repeat(${gridNumber}, lfr)`
-        container.style.gridTemplateRows = `repeat(${gridNumber}, lfr)`
-        container.insertAdjacentElement('beforeend', gridBox);
-    }
-
-    var gridPix = container.querySelectorAll('div');
-    gridPrix.forEach(gridPix => gridPix.addEventListener('mouseover', rgbGrid));
+/* This is the function that the mouseover calls to set the grid cell to black */
+function setBgColor() {
+    this.style.backgroundColor = "black";
 }
+/* */
+function createGridCells(cellsLength) {
+    const numOfCells = (cellsLength * cellsLength);
+    const widthOrHeight = `${(gridSize / cellsLength) - 2}px`;
+    for (let i = 0; i < numOfCells; i++) {
+        const gridCell = document.createElement("div");
 
-function rgbGrid() {
-    switch (color) {
-        case'rainbow':
-            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
-            this.classList.remove('gray');
-            break;
-        case 'gray':
-            if(this.style.backgroundColor.match(/rgba/)) {
-                let currentOpacity = Number(this.style.backgroundColor.slice(-4, -1));
-                if(currentOpacity <= 0.9) {
-                    this.style.backgroundColor = `rgba(0, 0, 0, ${currentOpacity + 0.1})`
-                    this.classList.add('gray');
-                }
-            } else if (this.classList == 'gray' && this.style.backgroundColor == 'rgb(0, 0, 0)') {
-                return;
-            } else {
-                this.style.backgroundColor = 'rgba(0, 0, 0, 0.1)';
-            
-            }
-                break;
-            case 'eraser':
-                this.style.backgroundColor= '#ffffff';
-                this.classList.remove('gray');
-                break;
-            case 'black':
-                this.style.backgroundColor = '#000000';
-                this.classList.remove('gray');
-                break;
-            default:
-                this.style.backgroundColor = color;
-                this.classList.remove('gray');
-                break;
+        gridCell.style.width = gridCell.style.height = widthOrHeight;
+        gridCell.classList.add("cell");
+
+        easArea.appendChild(gridCell);
+
+        gridCell.addEventListener("mouseover", setBgColor);
+
     }
 }
 
-// Clear Button
-function eraseAllColor() {
-    var gridPixels = container.querySelectorAll('div');
-    gridPixels.forEach(gridPixel => gridPixel.style.backgroundColor = '#ffffff');
+/* */
+function remGridCells () {
+    while (easArea.firstChild) {
+        easArea.removeChild(easArea.firstChild);
+    }
 }
 
-// Updates color variable when a color button is clicked
-function changeColor(event) {
-    switch (event.target.dataset.color) { 
-        case 'rainbow':
-            color = 'rainbow';
-            break;  
-        case 'gray':
-            color = 'gray';
-            break;
-        case 'eraser':
-            color = 'eraser';
-            break;
-        default:
-            color = 'black';
-            break;
-    } 
-}
 
-function pixelSize() {
-    let gridPixels = container.querySelectorAll('div');
-    gridPixels.forEach(gridPixel => gridPixel.remove());
-    createGrid(slider.value);
-}
-
-function userColorSelection(event) {
-    color = event.target.value;
-}
-
-function buttonHover() {
-    this.style.border = '1px solid #ffffff';
-}
-
-function buttonStandard() {
-    this.style.border = '1px solid #FF0000';
-}
-
-// On Page Load - default size
-createGrid(10);
-
-// Event Listeners
-clearButton.addEventListener('click', eraseAllColor);
-clearButton.addEventListener('mouseover', buttonHover);
-clearButton.addEventListener('mouseout', buttonStandard);
-colorButtons.forEach(colorButton => colorButton.addEventListener('click', changeColor));
-colorButtons.forEach(colorButton => colorButton.addEventListener('mouseover', buttonHover));
-colorButtons.forEach(colorButton => colorButton.addEventListener('mouseout', buttonStandard));
-slider.addEventListener('mouseup', pixelSize);
-userColorPicker.addEventListener('change', userColorSelection, false);
-userColorPicker.addEventListener('input', userColorSelection, false);
+createGridCells (16);
